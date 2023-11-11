f5ff7ab1f3ce0029763680db785080159ed27132
Tomás Mónaco <monacotomas99@gmail.com>
2023-11-11 13:24:15 +0100

    Added dates functionality

--- app/components/Calendar/Calendar.tsx
+++ app/components/Calendar/Calendar.tsx
@@ -1,20 +1,44 @@
+'use client';
+import moment from 'moment';
 import DateBox from '../DateBox/DateBox';
 import styles from './Calendar.module.scss';
+import { useState } from 'react';
 
 type CalendarProps = object;
 
 const Calendar: React.FC<CalendarProps> = () => {
+  const startOfWeek = moment().startOf('isoWeek');
+  const endOfWeek = moment().endOf('isoWeek');
+  const [selectedDayOfYear, setSelectedDayOfYear] = useState(
+    moment().dayOfYear(),
+  );
+
+  let calDays = [];
+  let day = startOfWeek;
+
+  while (day <= endOfWeek) {
+    calDays.push({
+      dateName: moment(day).format('ddd'),
+      dateNum: moment(day).format('DD'),
+      dateOfYear: moment(day).dayOfYear(),
+    });
+    day = day.clone().add(1, 'd');
+  }
+
   return (
     <div className={styles.calendarContainer}>
-      <div className={styles.month}>NOVEMBER 2023</div>
+      <div className={styles.month}>
+        {moment().format('MMMM YYYY').toUpperCase()}
+      </div>
       <div className={styles.datesWrapper}>
-        <DateBox />
-        <DateBox />
-        <DateBox selected />
-        <DateBox />
-        <DateBox />
-        <DateBox />
-        <DateBox />
+        {calDays.map((calDate, key) => (
+          <DateBox
+            key={key}
+            selected={selectedDayOfYear === calDate.dateOfYear ? true : false}
+            calDate={calDate}
+            setSelectedDate={setSelectedDayOfYear}
+          />
+        ))}
       </div>
     </div>
   );
--- app/components/DateBox/DateBox.tsx
+++ app/components/DateBox/DateBox.tsx
@@ -1,14 +1,30 @@
+import { Dispatch, SetStateAction } from 'react';
 import styles from './DateBox.module.scss';
+
+interface CalDate {
+  dateNum: string;
+  dateName: string;
+  dateOfYear: number;
+}
 
 type DateProps = {
   selected?: boolean;
+  calDate: CalDate;
+  setSelectedDate: Dispatch<SetStateAction<number>>;
 };
 
-const DateBox: React.FC<DateProps> = ({ selected }) => {
+const DateBox: React.FC<DateProps> = ({
+  selected,
+  calDate,
+  setSelectedDate,
+}) => {
   return (
-    <div className={`${styles.dateWrapper} ${selected ? styles.selected : ''}`}>
-      <span className="dateName">FRI</span>
-      <span className="dateNum">07</span>
+    <div
+      className={`${styles.dateWrapper} ${selected ? styles.selected : ''}`}
+      onClick={() => setSelectedDate(calDate.dateOfYear)}
+    >
+      <span className="dateName">{calDate.dateName.toUpperCase()}</span>
+      <span className="dateNum">{calDate.dateNum}</span>
     </div>
   );
 };
