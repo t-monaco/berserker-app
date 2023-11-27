6d1b039e68ebd7bce380b5c0325d4e673162635e
Tomás Mónaco <monacotomas99@gmail.com>
2023-11-27 22:38:38 +0100

    move viewport to Viewport object

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import StyledComponentsRegistry from '@/lib/registry';
 import { ClerkProvider } from '@clerk/nextjs';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Toaster } from 'react-hot-toast';
 import { drukFont } from './fonts/fonts';
 import './styles/globals.scss';
@@ -18,7 +18,12 @@ export const metadata: Metadata = {
   icons: {
     icon: '/favicon.ico',
   },
-  viewport: { width: 'device-width', initialScale: 1, maximumScale: 1 },
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
 };
 
 export default function RootLayout({
