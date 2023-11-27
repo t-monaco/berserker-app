6e14ee60f8e0b3002a42b0e070fb00e655c1d0ce
Tomás Mónaco <monacotomas99@gmail.com>
2023-11-27 12:27:01 +0100

    add calculator feature

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -5,7 +5,9 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { HiRefresh } from 'react-icons/hi';
+import { FaCalculator } from 'react-icons/fa';
 import * as Styled from './Header.styled';
+import PercentageCalculator from '../PercentageCalculator';
 
 type HeaderProps = {
   // TODO: if I used clerk's organization i can remove this props and get the role from the useUser
@@ -25,17 +27,34 @@ const Header: React.FC<HeaderProps> = ({ userRole }) => {
     }, 3000);
   };
 
+  const [isCalculatorOpen, setIsCalculatorOpen] = useState(false);
+
+  const showCalculatorModal = () => {
+    setIsCalculatorOpen(true);
+  };
+
+  const handleCancelCalculatorModal = () => {
+    setIsCalculatorOpen(false);
+  };
+
   return (
     <Styled.HeaderWrapper>
       <div className="month-year">
         {dayjs().format('MMMM YYYY').toUpperCase()}
         <div className="flex gap-4">
           {userRole === 'ADMIN' && <Link href="/admin">ADMIN</Link>}
+          <span className="icon-wrapper" onClick={() => showCalculatorModal()}>
+            <FaCalculator />
+          </span>
           <span className="icon-wrapper" onClick={() => handleRefresh()}>
             <HiRefresh className={rotate ? 'rotate' : ''} />
           </span>
         </div>
       </div>
+      <PercentageCalculator
+        isOpen={isCalculatorOpen}
+        onCancel={handleCancelCalculatorModal}
+      />
     </Styled.HeaderWrapper>
   );
 };
