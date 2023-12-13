674896f20c1a8ef7dbecf1ff6ab7c65640e1ccf7
Tomás Mónaco <monacotomas99@gmail.com>
2023-12-13 20:20:53 +0100

    PWA updates + not-found page

--- app/components/Generics/Generics.styled.tsx
+++ app/components/Generics/Generics.styled.tsx
@@ -1,16 +1,16 @@
 'use client';
 import styled from 'styled-components';
 
-export const BasicHero = styled.div<{ position: 'right' | 'left' }>`
+export const BasicHero = styled.div<{ $position: 'right' | 'left' }>`
   background-color: var(--primary-color);
-  border-radius: ${({ position }) =>
-    position === 'left'
+  border-radius: ${({ $position }) =>
+    $position === 'left'
       ? '7px 60px 60px 7px'
-      : position === 'right'
-      ? '60px 7px 7px 60px'
-      : '7px'};
+      : $position === 'right'
+        ? '60px 7px 7px 60px'
+        : '7px'};
   color: var(--secondary-font-color);
   cursor: pointer;
   padding: 36px 30px 36px 16px;
-  text-align: ${({ position }) => (position ? position : 'inherit')};
+  text-align: ${({ $position }) => ($position ? $position : 'inherit')};
 `;
