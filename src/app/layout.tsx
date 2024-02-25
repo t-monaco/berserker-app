6b879fcc9ad58e7ceca35e62b042d8f199eb7b35
Tomás Mónaco <monacotomas99@gmail.com>
2024-02-26 08:28:13 +1100

    Test splash screens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export const metadata: Metadata = {
     capable: true,
     title: 'Berserker Program',
     statusBarStyle: 'black-translucent',
+    startupImage: '/splashscreens/iphoneplus_splash.png',
   },
   manifest: '/manifest.json',
   icons: {
