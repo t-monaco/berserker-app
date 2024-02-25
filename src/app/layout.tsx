7ecaa1eb57a804d90c7ce57fc82f9b21df551bfa
Tomás Mónaco <monacotomas99@gmail.com>
2024-02-26 10:18:35 +1100

    Test splash screens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import { drukFont } from '../fonts/fonts';
 import Provider from '../trpc/Provider';
 import '../styles/globals.scss';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: 'Berserker Program',
@@ -16,7 +17,6 @@ export const metadata: Metadata = {
     capable: true,
     title: 'Berserker Program',
     statusBarStyle: 'black-translucent',
-    startupImage: '/splashscreens/iphoneplus_splash.png',
   },
   manifest: '/manifest.json',
   icons: {
@@ -44,6 +44,59 @@ export default function RootLayout({
       }}
     >
       <html lang="en">
+        <Head>
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <link
+            href="splashscreens/iphone5_splash.png"
+            media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="splashscreens/iphone6_splash.png"
+            media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="splashscreens/iphoneplus_splash.png"
+            media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="splashscreens/iphonex_splash.png"
+            media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="splashscreens/iphonexr_splash.png"
+            media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="splashscreens/iphonexsmax_splash.png"
+            media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="splashscreens/ipad_splash.png"
+            media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="splashscreens/ipadpro1_splash.png"
+            media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="splashscreens/ipadpro3_splash.png"
+            media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="splashscreens/ipadpro2_splash.png"
+            media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+        </Head>
         <body className={drukFont.className}>
           <ToastContainer
             position="top-center"
