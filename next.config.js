aaf3fad21973b5e74692cc4eff08f3783ea24921
Tomás Mónaco <monacotomas99@gmail.com>
2024-02-27 12:02:11 +1100

    remove unuse packages

--- next.config.js
+++ next.config.js
@@ -9,13 +9,6 @@ const withSerwist = require('@serwist/next').default({
   disable: process.env.NODE_ENV === 'development',
 });
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
